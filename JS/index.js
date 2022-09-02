2ba51a687d0d9f0902f541a8d336d465c4083d66
ihemebiriegondu <ihemebiriegondu@gmail.com>
2022-09-02 20:16:05 +0100

    added link to a tags

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,21 @@
-const offcanvasElementList = document.querySelectorAll('.offcanvas')
-const offcanvasList = [...offcanvasElementList].map(offcanvasEl => new bootstrap.Offcanvas(offcanvasEl))
+const moveTestimonials = (direction) => {
+    const scrollingTestimonials = document.querySelector(".scrolling-testimonials");
+    /*const cards = document.querySelector(".testimonials-cards");
+    const cardWidth = cards.width;*/
 
-const bsOffcanvas = new bootstrap.Offcanvas('#myOffcanvas')
+    scrollCompleted = 0;
+    let slideVar = setInterval(function () {
+        if (direction == 'left') {
+            scrollingTestimonials.scrollLeft -= 360;
+            scrollingTestimonials.style.scrollBehavior = "smooth"
+        } else {
+            scrollingTestimonials.scrollLeft += 360;
+            scrollingTestimonials.style.scrollBehavior = "smooth"
+        }
+        scrollCompleted += 360;
+        if (scrollCompleted >= 100) {
+            window.clearInterval(slideVar);
+            scrollingTestimonials.style.scrollBehavior = "smooth"
+        }
+    }, 30);
+}
