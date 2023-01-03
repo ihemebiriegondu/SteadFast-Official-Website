61594bdd4bf4b6a14e0721f76609a953a6f25a11
ihemebiriegondu <ihemebiriegondu@gmail.com>
2023-01-03 21:04:10 +0100

    finished the add event function

--- admins/showModals.js
+++ admins/showModals.js
@@ -7,7 +7,14 @@ const showEventBackdrop = () => {
 const showEditEventModal = (event) => {
     const button = event.target
 
-    document.querySelector(".eventImg").src = button.parentElement.children[3].src;
-    document.querySelector("#titleInput").value = button.parentElement.children[2].textContent;
-    document.querySelector("#dateInput").value = button.parentElement.children[4].textContent;
+    document.querySelector(".eventImg").src = button.parentElement.parentElement.children[3].src;
+    document.querySelector("#titleInput").value = button.parentElement.parentElement.children[2].textContent;
+    document.querySelector("#dateInput").value = button.parentElement.parentElement.children[4].textContent;
+}
+
+const showDeleteEventModal = (event) => {
+    const button = event.target
+
+    document.querySelector(".deleteEventButton").id = button.id
+    //console.log(document.querySelector(".deleteEventButton").id)
 }
