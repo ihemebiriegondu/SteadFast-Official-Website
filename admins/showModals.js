b048db9bb589eddf3e5ce241ccfffafc82cc74f3
ihemebiriegondu <ihemebiriegondu@gmail.com>
2023-01-04 21:41:35 +0100

    working on updating admin info

--- admins/showModals.js
+++ admins/showModals.js
@@ -32,12 +32,38 @@ const showEventBackdrop = (event) => {
 
 const showEditEventModal = (event) => {
     const button = event.target
+    const eventInputs = document.querySelectorAll(".eventInput");
+    const allInputs = document.querySelectorAll(".allInputs");
+
+    allInputs.forEach(input => {
+        input.classList.add("d-none")
+    })
+    eventInputs.forEach(input => {
+        input.classList.remove("d-none")
+    });
 
     document.querySelector(".eventImg").src = button.parentElement.parentElement.children[3].src;
     document.querySelector("#titleInput").value = button.parentElement.parentElement.children[2].textContent;
     document.querySelector("#dateInput").value = button.parentElement.parentElement.children[4].textContent;
 
     document.querySelector(".editEventForm").id = button.id
+}
+
+const showEditAdminModal = (event) => {
+    const button = event.target
+    const adminInputs = document.querySelectorAll(".adminInput");
+    const allInputs = document.querySelectorAll(".allInputs");
+
+    allInputs.forEach(input => {
+        input.classList.add("d-none")
+    })
+    adminInputs.forEach(input => {
+        input.classList.remove("d-none")
+    });
+
+    document.querySelector(".eventImg").src = button.parentElement.parentElement.children[4].src;
+    document.querySelector("#nameInput").value = button.parentElement.parentElement.children[1].textContent;
+    document.querySelector("#positionInput").value = button.parentElement.parentElement.children[2].textContent;
 }
 
 const showDeleteEventModal = (event) => {
