1178288c01131193dcbf1789cd85a015ecfa2d52
ihemebiriegondu <ihemebiriegondu@gmail.com>
2023-06-19 22:36:41 +0100

    working on news search function

--- JS/search.js
+++ JS/search.js
@@ -1,14 +1,16 @@
 // Get references to the search form, input, and results container
 const searchForm = document.getElementById('search-form');
-const searchInput = document.getElementById('search-input');
+const searchInput = document.getElementById('search-news');
+const searchForm2 = document.getElementById('search-form2');
+const searchInput2 = document.getElementById('search-news2');
 const searchResults = document.getElementById('search-results');
 
 // Add event listener to the form submission
-searchForm.addEventListener('submit', function (e) {
+searchForm2.addEventListener('submit', function (e) {
     e.preventDefault(); // Prevent form submission
 
     // Get the search query from the input field
-    const query = searchInput.value.trim();
+    const query = searchInput2.value.trim();
 
     performSearch(query)
 });
@@ -19,15 +21,17 @@ function performSearch(query) {
     // This could involve making an API call, searching through data, etc.
     // Return an array of search results
     // Example: return searchResultsArray;
-    const elements = document.getElementsByTagName("*"); // Get all elements on the webpage
+    const headlines = document.querySelectorAll('.headlines') // Get all headlines
 
-    for (let i = 0; i < elements.length; i++) {
-        const element = elements[i];
-        const textContent = element.textContent || element.innerText || ""; // Get the text content of the element
+    for (let i = 0; i < headlines.length; i++) {
+        const headline = headlines[i];
+        console.log(headline)
+        const textContent = headline.textContent || headline.innerText || ""; // Get the text content of the headline
+        console.log(textContent)
 
         if (textContent.includes(query)) {
-            element.classList.add("text-warning-emphasis"); // Add the specified class to the matching element
-            //console.log(element)
+            headline.classList.add("text-warning-emphasis"); // Add the specified class to the matching headline
+            //console.log(headline)
             console.log(textContent)
         }
     }
