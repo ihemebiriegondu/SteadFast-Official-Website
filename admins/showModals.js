db790a26c71d01edaef8b4a5afd61ebe3394760c
ihemebiriegondu <ihemebiriegondu@gmail.com>
2023-06-19 21:21:15 +0100

    added admin function to add, edit and delete news

--- admins/showModals.js
+++ admins/showModals.js
@@ -1,3 +1,4 @@
+//backdrops
 const hideAllEventBackdrop = (event) => {
     let allCanvases = document.querySelectorAll(".myoffcanvases");
     let navLinks = document.querySelectorAll(".nav-link")
@@ -30,10 +31,14 @@ const showEventBackdrop = (event) => {
     offcanvasTarget.classList.add("active")
 }
 
+
+//modals
 const showAddEventFunction = () => {
     let addForm = document.getElementById("addEventForm");
     const eventInputs = document.querySelectorAll(".eventInput");
     const allInputs = document.querySelectorAll(".allInputs");
+
+    document.getElementById('staticBackdropLabel2').textContent = "Add New Event"
 
     allInputs.forEach(input => {
         input.classList.add("d-none")
@@ -46,9 +51,10 @@ const showAddEventFunction = () => {
         document.querySelector(".img-div").classList.remove("d-none")
     }
 
-    if (addForm.classList.contains("addStaff") || addForm.classList.contains("addStudent")) {
+    if (addForm.classList.contains("addStaff") || addForm.classList.contains("addStudent") || addForm.classList.contains('addNews')) {
         addForm.classList.remove("addStaff")
         addForm.classList.remove("addStudent")
+        addForm.classList.remove('addNews')
         addForm.classList.add("addEvent")
     } else {
         addForm.classList.add("addEvent")
@@ -59,6 +65,8 @@ const showAddStaffFunction = () => {
     let addForm = document.getElementById("addEventForm");
     const staffInputs = document.querySelectorAll(".staffInput");
     const allInputs = document.querySelectorAll(".allInputs");
+
+    document.getElementById('staticBackdropLabel2').textContent = "Add Staff"
 
     allInputs.forEach(input => {
         input.classList.add("d-none")
@@ -71,9 +79,10 @@ const showAddStaffFunction = () => {
         document.querySelector(".img-div").classList.remove("d-none")
     }
 
-    if (addForm.classList.contains("addEvent") || addForm.classList.contains("addStudent")) {
+    if (addForm.classList.contains("addEvent") || addForm.classList.contains("addStudent") || addForm.classList.contains('addNews')) {
         addForm.classList.remove("addEvent")
         addForm.classList.remove("addStudent")
+        addForm.classList.remove('addNews')
         addForm.classList.add("addStaff")
     } else {
         addForm.classList.add("addStaff")
@@ -85,6 +94,8 @@ const showAddStudentFunction = () => {
     const studentInputs = document.querySelectorAll(".studentInput");
     const allInputs = document.querySelectorAll(".allInputs");
 
+    document.getElementById('staticBackdropLabel2').textContent = "Add Student"
+
     allInputs.forEach(input => {
         input.classList.add("d-none")
     })
@@ -94,12 +105,41 @@ const showAddStudentFunction = () => {
 
     document.querySelector(".img-div").classList.add("d-none")
 
-    if (addForm.classList.contains("addEvent") || addForm.classList.contains("addStaff")) {
+    if (addForm.classList.contains("addEvent") || addForm.classList.contains("addStaff") || addForm.classList.contains('addNews')) {
         addForm.classList.remove("addEvent")
         addForm.classList.remove("addStaff")
+        addForm.classList.remove('addNews')
         addForm.classList.add("addStudent")
     } else {
         addForm.classList.add("addStudent")
+    }
+}
+
+const showAddNewsFunction = () => {
+    let addForm = document.getElementById("addEventForm");
+    const newsInputs = document.querySelectorAll(".newsInput");
+    const allInputs = document.querySelectorAll(".allInputs");
+
+    document.getElementById('staticBackdropLabel2').textContent = "Add News"
+
+    allInputs.forEach(input => {
+        input.classList.add("d-none")
+    })
+    newsInputs.forEach(input => {
+        input.classList.remove("d-none")
+    });
+
+    if (document.querySelector(".img-div").classList.contains("d-none")) {
+        document.querySelector(".img-div").classList.remove("d-none")
+    }
+
+    if (addForm.classList.contains("addStaff") || addForm.classList.contains("addStudent") || addForm.classList.contains('addEvent')) {
+        addForm.classList.remove("addStaff")
+        addForm.classList.remove("addStudent")
+        addForm.classList.remove("addEvent")
+        addForm.classList.add('addNews')
+    } else {
+        addForm.classList.add('addNews')
     }
 }
 
@@ -150,9 +190,10 @@ const showEditAdminModal = (event) => {
     document.getElementById("staticBackdropLabel").textContent = "Edit Admin Info"
 
     document.querySelector(".editEventForm").id = button.id
-    if (editEventForm.classList.contains("eventz") || editEventForm.classList.contains("staffz")) {
+    if (editEventForm.classList.contains("eventz") || editEventForm.classList.contains("staffz") || editEventForm.classList.contains("newz")) {
         editEventForm.classList.remove("eventz")
         editEventForm.classList.remove("staffz")
+        editEventForm.classList.remove("newz")
         editEventForm.classList.add("adminz")
     } else {
         editEventForm.classList.add("adminz")
@@ -179,12 +220,42 @@ const showEditStaffModal = (event) => {
     document.getElementById("staticBackdropLabel").textContent = "Edit Staff Info"
 
     document.querySelector(".editEventForm").id = button.id
-    if (editEventForm.classList.contains("eventz") || editEventForm.classList.contains("adminz")) {
+    if (editEventForm.classList.contains("eventz") || editEventForm.classList.contains("adminz") || editEventForm.classList.contains("newz")) {
         editEventForm.classList.remove("eventz")
         editEventForm.classList.remove("adminz")
+        editEventForm.classList.remove("newz")
         editEventForm.classList.add("staffz")
     } else {
         editEventForm.classList.add("staffz")
+    }
+}
+
+const showEditNewsModal = (event) => {
+    const button = event.target
+    const newsInputs = document.querySelectorAll(".newsInput");
+    const allInputs = document.querySelectorAll(".allInputs");
+    const editEventForm = document.querySelector(".editEventForm")
+
+    allInputs.forEach(input => {
+        input.classList.add("d-none")
+    })
+    newsInputs.forEach(input => {
+        input.classList.remove("d-none")
+    });
+
+    document.querySelector(".eventImg").src = button.parentElement.parentElement.children[0].children[0].src;
+    document.querySelector("#headlineInput").value = button.parentElement.parentElement.children[0].children[1].children[0].textContent;
+    document.querySelector("#contentInput").value = button.parentElement.parentElement.children[0].children[1].children[2].textContent;
+    document.getElementById("staticBackdropLabel").textContent = "Edit News"
+
+    document.querySelector(".editEventForm").id = button.id
+    if (editEventForm.classList.contains("adminz") || editEventForm.classList.contains("staffz") || editEventForm.classList.contains("eventz")) {
+        editEventForm.classList.remove("adminz")
+        editEventForm.classList.remove("staffz")
+        editEventForm.classList.remove("eventz")
+        editEventForm.classList.add("newz")
+    } else {
+        editEventForm.classList.add("newz")
     }
 }
 
@@ -198,9 +269,10 @@ const showDeleteEventModal = (event) => {
     deleteQuestion.textContent = "Are you sure you want to delete this event?"
 
     let deleteEventButton = document.querySelector(".deleteEventButton");
-    if (deleteEventButton.classList.contains("setStaff") || deleteEventButton.classList.contains("setStudent")) {
+    if (deleteEventButton.classList.contains("setStaff") || deleteEventButton.classList.contains("setStudent") || deleteEventButton.classList.contains("setNews")) {
         deleteEventButton.classList.remove("setStaff")
         deleteEventButton.classList.remove("setStudent")
+        deleteEventButton.classList.remove("setNews")
         deleteEventButton.classList.add("setEvent")
     } else {
         deleteEventButton.classList.add("setEvent")
@@ -217,9 +289,10 @@ const showDeleteStaffModal = (event) => {
     deleteQuestion.textContent = "Are you sure you want to delete this staff info?"
 
     let deleteEventButton = document.querySelector(".deleteEventButton");
-    if (deleteEventButton.classList.contains("setEvent") || deleteEventButton.classList.contains("setStudent")) {
+    if (deleteEventButton.classList.contains("setEvent") || deleteEventButton.classList.contains("setStudent") || deleteEventButton.classList.contains("setNews")) {
         deleteEventButton.classList.remove("setEvent")
         deleteEventButton.classList.remove("setStudent")
+        deleteEventButton.classList.remove("setNews")
         deleteEventButton.classList.add("setStaff")
     } else {
         deleteEventButton.classList.add("setStaff")
@@ -236,11 +309,32 @@ const showDeleteStudentModal = (event) => {
     deleteQuestion.textContent = "Are you sure you want to delete this student info?"
 
     let deleteEventButton = document.querySelector(".deleteEventButton");
-    if (deleteEventButton.classList.contains("setEvent") || deleteEventButton.classList.contains("setStaff")) {
+    if (deleteEventButton.classList.contains("setEvent") || deleteEventButton.classList.contains("setStaff") || deleteEventButton.classList.contains("setNews")) {
         deleteEventButton.classList.remove("setEvent")
         deleteEventButton.classList.remove("setStaff")
+        deleteEventButton.classList.remove("setNews")
         deleteEventButton.classList.add("setStudent")
     } else {
         deleteEventButton.classList.add("setStudent")
+    }
+}
+
+const showDeleteNewsModal = (event) => {
+    const button = event.target
+
+    document.querySelector(".deleteEventButton").id = button.id
+    //console.log(document.querySelector(".deleteEventButton").id)
+
+    let deleteQuestion = document.querySelector(".deleteQuestion");
+    deleteQuestion.textContent = "Are you sure you want to delete this news?"
+
+    let deleteEventButton = document.querySelector(".deleteEventButton");
+    if (deleteEventButton.classList.contains("setEvent") || deleteEventButton.classList.contains("setStaff") || deleteEventButton.classList.contains("setStudent")) {
+        deleteEventButton.classList.remove("setEvent")
+        deleteEventButton.classList.remove("setStaff")
+        deleteEventButton.classList.remove("setStudent")
+        deleteEventButton.classList.add("setNews")
+    } else {
+        deleteEventButton.classList.add("setNews")
     }
 }
